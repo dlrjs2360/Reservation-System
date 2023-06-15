7e2af92190ef6cf4d572800b67bc6dbf68702559
dlrjs2360 <lhg3670@naver.com>
2023-06-15 15:55:01 +0900

    :feature: feat: 로그인 기능 추가

--- src/main/java/com/numble/reservationsystem/service/UserService.java
+++ src/main/java/com/numble/reservationsystem/service/UserService.java
@@ -1,10 +1,12 @@
 package com.numble.reservationsystem.service;
 
 import com.numble.reservationsystem.config.JwtProvider;
+import com.numble.reservationsystem.entity.Token;
 import com.numble.reservationsystem.entity.domain.User;
 import com.numble.reservationsystem.entity.dto.user.UserRequestDto;
 import com.numble.reservationsystem.entity.dto.user.UserResponseDto;
 import com.numble.reservationsystem.repository.UserRepository;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,12 +28,30 @@ public class UserService {
     private String SECRET_KEY;
 
     public UserResponseDto register(UserRequestDto requestDto) {
-        // 이미 존재하는 유저인지 확인 필요
+        // 이미 존재하는 유저인지 확인 예외처리 필요
         User user = User.toEntity(requestDto);
         user.encodePassword(passwordEncoder.encode(requestDto.getPassword()));
         return UserResponseDto.of(userRepository.save(user));
     }
 
+    public UserResponseDto login(UserRequestDto requestDto, HttpServletResponse response) {
+        User user = userRepository.findByEmail(requestDto.getEmail()).orElseThrow();
 
+        if (!isValidPassword(requestDto.getPassword(), user.getPassword())) {
+            return null; // 예외 처리 추가
+        }
 
+        // 액세스, 리프레쉬 토큰 생성
+        Token token = jwtProvider.generateJwtToken(user.getEmail(), SECRET_KEY);
+
+        // 헤더에 토큰 저장
+        jwtProvider.setHeaderAccessToken(response, token.getAccessToken());
+        jwtProvider.setHeaderRefreshToken(response, token.getRefreshToken());
+
+        return UserResponseDto.of(user);
+    }
+
+    public boolean isValidPassword(String input, String encoded) {
+        return passwordEncoder.matches(input, encoded);
+    }
 }
