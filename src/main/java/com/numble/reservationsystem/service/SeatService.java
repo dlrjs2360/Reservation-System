f93837d57c9d344d9c9a3828e3e5406439a6ae56
dlrjs2360 <lhg3670@naver.com>
2023-06-18 02:33:59 +0900

    :sparkles: feat: 좌석 조회 기능 추가

--- src/main/java/com/numble/reservationsystem/service/SeatService.java
+++ src/main/java/com/numble/reservationsystem/service/SeatService.java
@@ -26,6 +26,12 @@ public class SeatService {
     private final SeatRepository seatRepository;
     private final ShowRepository showRepository;
 
+    /*
+     * 좌석 생성 기능
+     * 1. 좌석 번호는 문자형태
+     * 2. 등록한 시점에는 예약가능 상태? -> 공연이 오픈되어야 예약 가능 상태가 된다.
+     * 3. 후에 좌석 예약시에는 AVAILABLE만 예약이 가능하다.
+     * */
     public List<SeatResponseDto> registerSeats(SeatRegisterDto seatRegisterDto) {
         // 공연 관리자만 가능해야 함.
         Map<String, SeatType> seatTypeInfo = seatRegisterDto.getSeatTypeInfo();
@@ -39,7 +45,7 @@ public class SeatService {
                 Seat seat = Seat.builder()
                     .type(seatTypeInfo.get(entry.getKey()))
                     .number(entry.getKey() + String.valueOf(i))
-                    .status(SeatStatus.AVAILABLE)
+                    .status(SeatStatus.FORBIDDEN)
                     .show(show)
                     .build();
                 // 좌석 저장
@@ -53,5 +59,25 @@ public class SeatService {
         return seatList;
     }
 
+    /*
+     * 좌석 수정 기능
+     * 1. 이미 좌석 번호가 있으면 좌석 번호는 수정 불가능
+     * 2. 좌석 타입 변경 가능
+     * 3. 공연 ID 변경 불가능
+     * */
 
+    /*
+     * 좌석 삭제 기능
+     * 1. 이미 예매된 좌석 즉, Status가 BOOKED면 삭제 불가능
+     * 2. 공연이 삭제되면 모든 좌석도 삭제되어야 함.
+     * */
+
+
+
+    /*
+    * 좌석 조회 기능
+    * */
+    public SeatResponseDto getSeatRepository(Long seadId) {
+        return SeatResponseDto.of(seatRepository.findById(seadId).orElseThrow());
+    }
 }
